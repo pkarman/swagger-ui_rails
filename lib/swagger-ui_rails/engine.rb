6a1ee0069929883eb41c93ee39f4066efb97df96
Peter Karman <peter.karman@gsa.gov>
2016-01-21 10:19:07 -0600

    comment out reset.css until we can figure out a configurable way to ignore it

--- lib/swagger-ui_rails/engine.rb
+++ lib/swagger-ui_rails/engine.rb
@@ -5,7 +5,7 @@ module Swagger
       Rails.application.config.assets.precompile += %w{ swagger-ui.js }
       Rails.application.config.assets.precompile += %w{ swagger-oauth.js }
       Rails.application.config.assets.precompile += %w{ swagger-ui.css }
-      Rails.application.config.assets.precompile += %w{ swagger-ui/reset.css }
+      #Rails.application.config.assets.precompile += %w{ swagger-ui/reset.css }
       Rails.application.config.assets.precompile += %w{ swagger-ui/screen.css }
       Rails.application.config.assets.paths << root.join("app", "assets")
     end
